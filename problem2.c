de8b78059885363cefd67e6b968fcefe6a245283
marwasameh2003 <marwamostafa322@gmail.com>
2024-03-17 01:49:29 +0200

    added reading input from file

--- problem2.c
+++ problem2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "mpi.h"
 
 char Encrypt(char c) {
@@ -14,11 +15,14 @@ int main(int argc, char *argv[]) {
     int my_rank;
     int p;
     int range;
+    FILE *file;
     char str[200];
+    char fileName[200];
     int start, end;
     char temp[200]; // Use an appropriate size for temporary substring
     char result[400] = "";
     int option;
+    int inputOption;
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
@@ -26,22 +30,53 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
     if (my_rank == 0) {
+        printf("If you want to read from file enter 1 otherwise enter 2 \n");
+        scanf("%d", &inputOption);
+        if(inputOption == 2)
+        {
         printf("Enter the String: \n");
         scanf("%s", str);
         getchar();
+        }
+        else 
+        {
+        printf("Enter the file name: \n");
+        scanf("%s", fileName);
+        file = fopen(fileName, "r");
+
+        if (file == NULL) {
+            perror("Error opening the file");
+            return EXIT_FAILURE;
+        }
+         if (fgets(str, 200, file) != NULL) {
+        // Remove newline character if present
+        if (str[strlen(str) - 1] == '\n')
+            str[strlen(str) - 1] = '\0';
+        
+        printf("String read from file: %s\n", str);
+        }
+         else {
+            printf("Error reading from file.\n");
+            return EXIT_FAILURE;
+        }
+
+        // Close the file
+        fclose(file);
+
+        }
         printf("If you want to encrypt enter 1 otherwise enter 2 \n");
         scanf("%d", &option);
 
         int str_length = strlen(str);
         range = (str_length + (p - 2)) / (p - 1);
-
-        for (int i = 1; i < p; i++) {
+        int i;
+        for (i = 1; i < p; i++) {
             MPI_Send(&range, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&option, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(str + (i - 1) * range, range, MPI_CHAR, i, 0, MPI_COMM_WORLD);
         }
 
-        for (int i = 1; i < p; i++) {
+        for ( i = 1; i < p; i++) {
             MPI_Recv(temp, range, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             strcat(result, temp);
         }
@@ -56,7 +91,8 @@ int main(int argc, char *argv[]) {
         MPI_Recv(temp, range, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         // Process the substring
-        for (int i = 0; i < range && temp[i] != '\0'; i++) {
+        int i;
+        for ( i = 0; i < range && temp[i] != '\0'; i++) {
             if (option == 1) {
                 temp[i] = Encrypt(temp[i]);
             } else {
